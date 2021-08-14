84385183021f009543d5d1f79dea2f9077664e76
Dharmadurai Muthusamy <dharmadurai@autonom8.com>
2021-08-14 17:23:05 +0530

    ravi login react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState } from 'react';
+import axios  from 'axios';
 
 function App() {
+  const [loginDetails, setloginDetails] = useState({email:'',password:''})
+  const onChangeHandler = (e)=> {
+    
+    if(e.target.name === 'email')setloginDetails({...loginDetails,email:e.target.value})
+    if(e.target.name === 'password')setloginDetails({...loginDetails,password:e.target.value})
+  }
+
+  const loginRegister = async() => {
+  if(!loginDetails.email||!loginDetails.password) {alert('please provide all details');return}
+    // 'http://localhost:7000/'
+    const login = await axios.post('http://localhost:7000/login',{email:loginDetails.email,password:loginDetails.password});
+    console.log(login)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onChange={onChangeHandler}>
+        
+        <input className='input'type='email' name='email' defaultValue='' value={loginDetails.email}/>
+        <input className='input'type='password' name='password' defaultValue='' password={loginDetails.password}/>
+        <input type='button' name='login' value='Login' onClick={loginRegister}/>
+        
     </div>
   );
-}
+ }
 
 export default App;
